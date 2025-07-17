52473ff3454d9de0ee24badc3a0410605fbc336e
Sanket Saikia <sanketsaikia13@gmail.com>
2025-07-17 15:44:02 +0530

    quick start

---
+++ e2e-tests/playwright/e2e/plugins/quick-start.spec.ts
@@ -0,0 +1,46 @@
+import { expect, test } from "@playwright/test";
+import { Common } from "../../utils/common";
+import { UIhelper } from "../../utils/ui-helper";
+
+test.describe("Test Quick Start plugin", () => {
+  let uiHelper: UIhelper;
+  let common: Common;
+
+  test.beforeEach(async ({ page }) => {
+    common = new Common(page);
+    // await common.loginAsKeycloakUser();
+    await common.loginAsGuest();
+
+    uiHelper = new UIhelper(page);
+  });
+
+  test("Access Quick start from Global Header", async ({ page }) => {
+    await uiHelper.clickButtonByLabel("Help");
+    await uiHelper.clickByDataTestId("quickstart-button");
+    await uiHelper.clickButtonByLabel("Help");
+    await uiHelper.clickByDataTestId("quickstart-button");
+    await uiHelper.verifyText("Not started");
+    await uiHelper.verifyText("Let's get you started with Developer Hub");
+    await uiHelper.verifyText("We'll guide you through a few quick steps");
+    await uiHelper.clickButtonByText("Set up authentication");
+    await uiHelper.verifyButtonURL(
+      "Learn more",
+      "https://docs.redhat.com/en/documentation/red_hat_developer_hub/latest/html/authentication_in_red_hat_developer_hub/",
+    );
+    await uiHelper.clickButtonByText("Configure RBAC");
+    await uiHelper.verifyButtonURL("Manage access", "/rbac");
+    await uiHelper.clickButtonByText("Configure Git");
+    await uiHelper.verifyButtonURL(
+      "Learn more",
+      "https://docs.redhat.com/en/documentation/red_hat_developer_hub/latest/html/integrating_red_hat_developer_hub_with_github/",
+    );
+    await uiHelper.clickButtonByText("Manage plugins");
+    await uiHelper.verifyButtonURL("Explore plugins", "/extensions");
+    await uiHelper.clickButtonByText("Explore plugins");
+    await uiHelper.verifyText("Catalog");
+    await uiHelper.verifyText(/Plugins \((\d+)\)/);
+    await uiHelper.verifyText("25% progress");
+    await uiHelper.clickButton("Hide");
+    await expect(page.getByText("25% progress")).not.toBeVisible();
+  });
+});
