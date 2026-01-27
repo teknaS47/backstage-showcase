be87c0b49587db255a61f9e5a43988adcb4ae71e
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2026-01-27 15:42:04 +0100

    [release-1.9] fix(e2e): re-enable rbac tests (#4073)
    * fix(e2e): re-enable rbac tests
    
    Signed-off-by: Patrick Knight <pknight@redhat.com>
    
    * fix(e2e): add dynamic home page plugin to rbac values file
    
    Signed-off-by: Patrick Knight <pknight@redhat.com>
    
    * fix(e2e): add config values for rbac plugin
    
    Signed-off-by: Patrick Knight <pknight@redhat.com>
    
    ---------
    
    Signed-off-by: Patrick Knight <pknight@redhat.com>
    Co-authored-by: Patrick Knight <pknight@redhat.com>

--- e2e-tests/playwright/e2e/plugins/rbac/rbac.spec.ts
+++ e2e-tests/playwright/e2e/plugins/rbac/rbac.spec.ts
@@ -320,8 +320,7 @@ test.describe("Test RBAC", () => {
       await uiHelper.clickLink("RBAC");
     });
 
-    //FIXME RHDHBUGS-2483
-    test.skip("Create and edit a role from the roles list page", async ({
+    test("Create and edit a role from the roles list page", async ({
       page,
     }) => {
       const uiHelper = new UIhelper(page);
@@ -403,8 +402,7 @@ test.describe("Test RBAC", () => {
       await rbacPo.deleteRole("role:default/test-role");
     });
 
-    //FIXME https://issues.redhat.com/browse/RHDHBUGS-2483
-    test.skip("Edit users and groups and update policies of a role from the overview page", async ({
+    test("Edit users and groups and update policies of a role from the overview page", async ({
       page,
     }) => {
       const uiHelper = new UIhelper(page);
@@ -749,8 +747,7 @@ test.describe("Test RBAC", () => {
       await uiHelper.verifyHeading("All roles (1)");
     });
 
-    //FIXME https://issues.redhat.com/browse/RHDHBUGS-2483
-    test.skip("Test that user with `IsOwner` condition can access the RBAC page, create a role, edit a role, and delete the role", async ({
+    test("Test that user with `IsOwner` condition can access the RBAC page, create a role, edit a role, and delete the role", async ({
       page,
     }) => {
       const common = new Common(page);
