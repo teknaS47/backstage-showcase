9aff7b44e3909403f57eb2a8fa8e824794a17194
Sanket Saikia <sanketsaikia13@gmail.com>
2026-01-28 10:10:24 +0530

    fix

--- e2e-tests/playwright/data/theme-constants.ts
+++ e2e-tests/playwright/data/theme-constants.ts
@@ -24,20 +24,20 @@ export class ThemeConstants {
       navigationIndicatorColor: "rgb(45, 113, 190)",
     };
 
-    const lightDynamic: ThemeInfo = {
-      name: "Light Dynamic",
-      primaryColor: "rgb(255, 95, 21)",
-      headerColor1: "rgb(248, 248, 248)",
-      headerColor2: "rgb(248, 248, 248)",
-      navigationIndicatorColor: "rgb(255, 95, 21)",
-    };
-
     const darkDynamic: ThemeInfo = {
       name: "Dark Dynamic",
       primaryColor: "#ab75cf",
       headerColor1: "rgb(0, 0, 208)",
       headerColor2: "rgb(255, 246, 140)",
       navigationIndicatorColor: "rgb(244, 238, 169)",
+    };
+
+    const lightDynamic: ThemeInfo = {
+      name: "Light Dynamic",
+      primaryColor: "rgb(255, 95, 21)",
+      headerColor1: "rgb(248, 248, 248)",
+      headerColor2: "rgb(248, 248, 248)",
+      navigationIndicatorColor: "rgb(255, 95, 21)",
     };
 
     return [light, dark, lightDynamic, darkDynamic];
