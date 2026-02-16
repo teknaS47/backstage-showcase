97bf131bc9466b6f9a80a41dec6b7bb1e6cb8d9e
ssaikia <sanketsaikia13@gmail.com>
2026-02-16 15:55:23 +0530

    re-enabled tests

--- e2e-tests/playwright/e2e/plugins/orchestrator/failswitch-workflow.spec.ts
+++ e2e-tests/playwright/e2e/plugins/orchestrator/failswitch-workflow.spec.ts
@@ -7,8 +7,10 @@ import { JOB_NAME_PATTERNS } from "../../../utils/constants";
 import { LogUtils } from "../../audit-log/log-utils";
 
 test.describe("Orchestrator failswitch workflow tests", () => {
-  // TODO: https://issues.redhat.com/browse/RHDHBUGS-2184 fix orchestrator tests on Operator deployment
-  test.fixme(() => skipIfJobName(JOB_NAME_PATTERNS.OPERATOR));
+  test.skip(() => skipIfJobName(JOB_NAME_PATTERNS.OSD_GCP)); // skipping orchestrator tests on OSD-GCP due to infra not being installed
+  test.skip(() => skipIfJobName(JOB_NAME_PATTERNS.GKE)); // skipping orchestrator tests on GKE - plugins disabled
+  test.skip(() => skipIfJobName(JOB_NAME_PATTERNS.AKS)); // skipping orchestrator tests on AKS - plugins disabled
+  test.skip(() => skipIfJobName(JOB_NAME_PATTERNS.EKS)); // skipping orchestrator tests on EKS - plugins disabled
 
   let uiHelper: UIhelper;
   let common: Common;
