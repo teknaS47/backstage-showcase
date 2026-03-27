5e8248cfcea9d9297b2ee3a466506b2528d435b2
Jan Richter <jrichter@redhat.com>
2026-03-27 13:45:52 +0100

    fix(ci): update backstage customization provider image stream (#4485)

--- .ci/pipelines/utils.sh
+++ .ci/pipelines/utils.sh
@@ -510,8 +510,8 @@ deploy_test_backstage_customization_provider() {
   # Check if the buildconfig already exists
   if ! oc get buildconfig test-backstage-customization-provider -n "${project}" > /dev/null 2>&1; then
     echo "Creating new app for test-backstage-customization-provider"
-    oc new-app -S openshift/nodejs:18-minimal-ubi8
-    oc new-app https://github.com/janus-qe/test-backstage-customization-provider --image-stream="openshift/nodejs:18-ubi8" --namespace="${project}"
+    oc new-app -S openshift/nodejs:20-ubi8
+    oc new-app https://github.com/janus-qe/test-backstage-customization-provider --image-stream="openshift/nodejs:20-ubi8" --namespace="${project}"
   else
     echo "BuildConfig for test-backstage-customization-provider already exists in ${project}. Skipping new-app creation."
   fi
